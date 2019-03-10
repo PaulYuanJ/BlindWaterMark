b9444d95f5468f9a0900984934c9ac1cd2f35eb5
mlge <admin@mlge.xyz>
2019-03-10 23:57:14 +0800

    update python2 to python3

--- bwm.py
+++ bwm.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf8 -*-
 
 import sys
@@ -11,20 +10,20 @@ alpha = 3.0
 
 if __name__ == '__main__':
     if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) < 2:
-        print 'Usage: python bwm.py <cmd> [arg...] [opts...]'
-        print '  cmds:'
-        print '    encode <image> <watermark> <image(encoded)>'
-        print '           image + watermark -> image(encoded)'
-        print '    decode <image> <image(encoded)> <watermark>'
-        print '           image + image(encoded) -> watermark'
-        print '  opts:'
-        print '    --debug,          Show debug'
-        print '    --seed <int>,     Manual setting random seed (default is 20160930)'
-        print '    --alpha <float>,  Manual setting alpha (default is 3.0)'
+        print('Usage: python bwm.py <cmd> [arg...] [opts...]')
+        print('  cmds:')
+        print('    encode <image> <watermark> <image(encoded)>')
+        print('           image + watermark -> image(encoded)')
+        print('    decode <image> <image(encoded)> <watermark>')
+        print('           image + image(encoded) -> watermark')
+        print('  opts:')
+        print('    --debug,          Show debug')
+        print('    --seed <int>,     Manual setting random seed (default is 20160930)')
+        print('    --alpha <float>,  Manual setting alpha (default is 3.0)')
         sys.exit(1)
     cmd = sys.argv[1]
     if cmd != 'encode' and cmd != 'decode':
-        print 'Wrong cmd %s' % cmd
+        print('Wrong cmd %s' % cmd)
         sys.exit(1)
     if '--debug' in sys.argv:
         debug = True
@@ -32,7 +31,7 @@ if __name__ == '__main__':
     if '--seed' in sys.argv:
         p = sys.argv.index('--seed')
         if len(sys.argv) <= p+1:
-            print 'Missing <int> for --seed'
+            print('Missing <int> for --seed')
             sys.exit(1)
         seed = int(sys.argv[p+1])
         del sys.argv[p+1]
@@ -40,13 +39,13 @@ if __name__ == '__main__':
     if '--alpha' in sys.argv:
         p = sys.argv.index('--alpha')
         if len(sys.argv) <= p+1:
-            print 'Missing <float> for --alpha'
+            print('Missing <float> for --alpha')
             sys.exit(1)
         alpha = float(sys.argv[p+1])
         del sys.argv[p+1]
         del sys.argv[p]
     if len(sys.argv) < 5:
-        print 'Missing arg...'
+        print('Missing arg...')
         sys.exit(1)
     fn1 = sys.argv[2]
     fn2 = sys.argv[3]
@@ -63,7 +62,7 @@ def bgr_to_rgb(img):
     return cv2.merge([r, g, b])
 
 if cmd == 'encode':
-    print 'image<%s> + watermark<%s> -> image(encoded)<%s>' % (fn1, fn2, fn3)
+    print('image<%s> + watermark<%s> -> image(encoded)<%s>' % (fn1, fn2, fn3))
     img = cv2.imread(fn1)
     wm = cv2.imread(fn2)
 
@@ -73,27 +72,27 @@ if cmd == 'encode':
         plt.subplot(234), plt.imshow(bgr_to_rgb(wm)), plt.title('watermark')
         plt.xticks([]), plt.yticks([])
 
-    # print img.shape # 高, 宽, 通道
+    # print(img.shape # 高, 宽, 通道)
     h, w = img.shape[0], img.shape[1]
     hwm = np.zeros((int(h * 0.5), w, img.shape[2]))
     assert hwm.shape[0] > wm.shape[0]
     assert hwm.shape[1] > wm.shape[1]
     hwm2 = np.copy(hwm)
-    for i in xrange(wm.shape[0]):
-        for j in xrange(wm.shape[1]):
+    for i in range(wm.shape[0]):
+        for j in range(wm.shape[1]):
             hwm2[i][j] = wm[i][j]
 
     random.seed(seed)
-    m, n = range(hwm.shape[0]), range(hwm.shape[1])
+    m, n = list(range(hwm.shape[0])), list(range(hwm.shape[1]))
     random.shuffle(m)
     random.shuffle(n)
-    for i in xrange(hwm.shape[0]):
-        for j in xrange(hwm.shape[1]):
+    for i in range(hwm.shape[0]):
+        for j in range(hwm.shape[1]):
             hwm[i][j] = hwm2[m[i]][n[j]]
 
     rwm = np.zeros(img.shape)
-    for i in xrange(hwm.shape[0]):
-        for j in xrange(hwm.shape[1]):
+    for i in range(hwm.shape[0]):
+        for j in range(hwm.shape[1]):
             rwm[i][j] = hwm[i][j]
             rwm[rwm.shape[0] - i - 1][rwm.shape[1] - j - 1] = hwm[i][j]
 
@@ -118,12 +117,12 @@ if cmd == 'encode':
     # 这里计算下保存前后的(溢出)误差
     img_wm2 = cv2.imread(fn3)
     sum = 0
-    for i in xrange(img_wm.shape[0]):
-        for j in xrange(img_wm.shape[1]):
-            for k in xrange(img_wm.shape[2]):
+    for i in range(img_wm.shape[0]):
+        for j in range(img_wm.shape[1]):
+            for k in range(img_wm.shape[2]):
                 sum += np.power(img_wm[i][j][k] - img_wm2[i][j][k], 2)
     miss = np.sqrt(sum) / (img_wm.shape[0] * img_wm.shape[1] * img_wm.shape[2]) * 100
-    print 'Miss %s%% in save' % miss
+    print('Miss %s%% in save' % miss)
 
     if debug:
         plt.subplot(233), plt.imshow(bgr_to_rgb(np.uint8(img_wm))), \
@@ -135,11 +134,11 @@ if cmd == 'encode':
     rwm = np.real(rwm)
 
     wm = np.zeros(rwm.shape)
-    for i in xrange(int(rwm.shape[0] * 0.5)):
-        for j in xrange(rwm.shape[1]):
+    for i in range(int(rwm.shape[0] * 0.5)):
+        for j in range(rwm.shape[1]):
             wm[m[i]][n[j]] = np.uint8(rwm[i][j])
-    for i in xrange(int(rwm.shape[0] * 0.5)):
-        for j in xrange(rwm.shape[1]):
+    for i in range(int(rwm.shape[0] * 0.5)):
+        for j in range(rwm.shape[1]):
             wm[rwm.shape[0] - i - 1][rwm.shape[1] - j - 1] = wm[i][j]
 
     if debug:
@@ -151,7 +150,7 @@ if cmd == 'encode':
         plt.show()
 
 elif cmd == 'decode':
-    print 'image<%s> + image(encoded)<%s> -> watermark<%s>' % (fn1, fn2, fn3)
+    print('image<%s> + image(encoded)<%s> -> watermark<%s>' % (fn1, fn2, fn3))
     img = cv2.imread(fn1)
     img_wm = cv2.imread(fn2)
 
@@ -162,7 +161,7 @@ elif cmd == 'decode':
         plt.xticks([]), plt.yticks([])
 
     random.seed(seed)
-    m, n = range(int(img.shape[0] * 0.5)), range(img.shape[1])
+    m, n = list(range(int(img.shape[0] * 0.5))), list(range(img.shape[1]))
     random.shuffle(m)
     random.shuffle(n)
 
@@ -186,11 +185,11 @@ elif cmd == 'decode':
         plt.xticks([]), plt.yticks([])
 
     wm = np.zeros(rwm.shape)
-    for i in xrange(int(rwm.shape[0] * 0.5)):
-        for j in xrange(rwm.shape[1]):
+    for i in range(int(rwm.shape[0] * 0.5)):
+        for j in range(rwm.shape[1]):
             wm[m[i]][n[j]] = np.uint8(rwm[i][j])
-    for i in xrange(int(rwm.shape[0] * 0.5)):
-        for j in xrange(rwm.shape[1]):
+    for i in range(int(rwm.shape[0] * 0.5)):
+        for j in range(rwm.shape[1]):
             wm[rwm.shape[0] - i - 1][rwm.shape[1] - j - 1] = wm[i][j]
     assert cv2.imwrite(fn3, wm)
 
